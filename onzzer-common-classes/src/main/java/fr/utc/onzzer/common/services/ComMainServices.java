cf82b424254d0a03e807e22aaf30c0ce99f38bee
Valentin Fagnet <valentin.fagnet@etu.utc.fr>
2023-11-22 19:07:25 +0100

    Changed connect method from ComMainServices interface (added 2 attributes: ip and port)

--- onzzer-common-classes/src/main/java/fr/utc/onzzer/common/services/ComMainServices.java
+++ onzzer-common-classes/src/main/java/fr/utc/onzzer/common/services/ComMainServices.java
@@ -4,10 +4,11 @@ import fr.utc.onzzer.common.dataclass.Track;
 import fr.utc.onzzer.common.dataclass.UserLite;
 
 import java.net.ConnectException;
+import java.net.InetAddress;
 import java.util.List;
 
 public interface ComMainServices {
-    void connect(UserLite user, List<Track> trackList) throws ConnectException;
+    void connect(UserLite user, List<Track> trackList, String ip, int port) throws ConnectException;
     void editUser(UserLite user) throws Exception;
     void disconnect() throws Exception;
 }
